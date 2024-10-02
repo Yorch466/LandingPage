24c499ef78eeba8c969bbc2aac60ba123bfa3bb6
Yorch466 <jorgellave77@gmail.com>
2024-10-01 23:53:35 -0400

    redirection to chalitaoe added

--- src/components/cards/ProyectsC.jsx
+++ src/components/cards/ProyectsC.jsx
@@ -7,7 +7,7 @@ export const ProyectsC = ({ img, tittle }) => {
             <div className="cardp-cont">
                 <img className="" src={capI} alt="no furulo we" />
                 <img className="cardp-cont-pry" src={img} alt="no furulo we" />
-                <a href="localhost:5173" target="_blank"><span>{tittle}</span></a>
+                <a href="https://chaliteoeapp.netlify.app/" target="_blank"><span>{tittle}</span></a>
             </div>
         </>
 
